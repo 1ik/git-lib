15738260155c0a13db5f7c2be7286a39e94ffd3b
anik <anik.hasan@blackswan.com>
2018-03-22 18:45:59 +0600

    adds support for commiting and adding all files

--- g.sh
+++ g.sh
@@ -2,10 +2,15 @@
 
 _g () {
  branch=$(git rev-parse --abbrev-ref HEAD)
+ arg=$1
 
  if [ "$arg" = "p" ]; then
   p="git push -u origin $branch"  
   eval $p 
+ elif [ "$arg" = "c" ]; then
+  msg=$2
+  cmd="git add --all && git commit -m '$msg'"
+  eval $cmd 
  else
   echo 'wow not found' 
  fi
